4248232f0af466d1370e17f4f51225c39a1d40ec
santio89 <santiolais1989@gmail.com>
2021-12-18 17:25:24 -0300

    feat-color theme picker

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /* INICIO cerrar nav burger si hago click fuera del area */
 
-var nav__anchor = document.getElementsByClassName("nav__anchor");
-var header__burgerMenu = document.getElementById("header__burgerMenu");
+let nav__anchor = document.getElementsByClassName("nav__anchor");
+let header__burgerMenu = document.getElementById("header__burgerMenu");
 
 /* cerrar nav burger al hacer click en un link (util para single page layouts) */
 for (let i=0; i < nav__anchor.length; i++)
@@ -13,8 +13,8 @@ for (let i=0; i < nav__anchor.length; i++)
 /* fin cerrar nav burger al hacer click en un link (util para single page layouts) */
 
 /* agregar EventListener cuando el nav burger sea abierto */
-var nav = document.getElementById("nav");
-var header__burgerIcon__icon = document.getElementById("header__burgerIcon__icon");
+let nav = document.getElementById("nav");
+let header__burgerIcon__icon = document.getElementById("header__burgerIcon__icon");
 
 header__burgerMenu.addEventListener( 'click', function(){
     if( this.checked ) {
@@ -24,7 +24,7 @@ header__burgerMenu.addEventListener( 'click', function(){
 /* fin agregar EventListener cuando el nav burger sea abierto */
 
 /* cerrar nav si hago click fuera del nav o del icono burger */
-var listener = function( e ) {
+let listener = function( e ) {
 if( e.target != header__burgerMenu && e.target != nav && e.target != header__burgerIcon__icon) {
     header__burgerMenu.checked = false;
     document.removeEventListener( 'click', listener );
@@ -41,3 +41,61 @@ window.addEventListener('resize', function() {
   }
 }, true);
 /* FIN cerrar burger nav si estaba abierto y hago resize hasta un tamaño en el que ya no existe */
+
+
+
+/* INICIO COLOR PICKER */
+
+let colorA = document.getElementById("colorA");
+let colorB = document.getElementById("colorB");
+let colorC = document.getElementById("colorC");
+let colorContainer = document.getElementById("colorContainer");
+let footer = document.getElementById("footer");
+let footer__hr = document.getElementById("footer__hr")
+let particles = document.getElementById("particles-js")
+let root = document.documentElement;
+
+colorA.onclick = function(){
+  colorC.classList.remove("color--active");
+  colorB.classList.remove("color--active");
+  colorA.classList.add("color--active");
+  root.style.setProperty("--color-primero","rgb(95, 0, 185)");
+  root.style.setProperty("--color-segundo","rgb(117, 255, 186)");
+  root.style.setProperty("--color-primero-rgb","95, 0, 185");
+  root.style.setProperty("--color-segundo-rgb","117, 255, 186");
+  
+  footer.style.setProperty("background", "linear-gradient(45deg, #2b0053, #0fff87)");
+  footer.style.setProperty("background-size", "500%");
+  footer__hr.style.setProperty("background", "linear-gradient(270deg, #2b0053, #0fff87)");
+  particles.style.setProperty("background", "linear-gradient(135deg, rgba(43, 0, 83, 0.8), rgba(15, 255, 135, 0.8))");
+  particles.style.setProperty("background-size", "500%");
+  root.style.setProperty("filter", "none");
+}
+
+colorB.onclick = function(){
+    colorA.classList.remove("color--active");
+    colorC.classList.remove("color--active");
+    colorB.classList.add("color--active");
+    root.style.setProperty("--color-primero","rgb(124, 47, 0)");
+    root.style.setProperty("--color-segundo","rgb(87, 216, 255)");
+    root.style.setProperty("--color-primero-rgb","124, 47, 0");
+    root.style.setProperty("--color-segundo-rgb","87, 216, 255");
+    
+    footer.style.setProperty("background", "linear-gradient(45deg, #160800, #00b8f0)");
+    footer.style.setProperty("background-size", "500%");
+    footer__hr.style.setProperty("background", "linear-gradient(270deg, #160800, #00b8f0)");
+    particles.style.setProperty("background", "linear-gradient(135deg, rgba(22, 8, 0, 0.8), rgba(0, 184, 240, 0.8))")
+    particles.style.setProperty("background-size", "500%");
+    root.style.setProperty("filter", "none");
+}
+
+colorC.onclick = function(){
+  colorA.classList.remove("color--active");
+  colorB.classList.remove("color--active");
+  colorC.classList.add("color--active");
+  root.style.setProperty("--color-primero","rgb(0, 0, 0)");
+  root.style.setProperty("--color-segundo","rgb(255, 255, 255)");
+  root.style.setProperty("--color-primero-rgb","0, 0, 0");
+  root.style.setProperty("--color-segundo-rgb","255, 255, 255");
+  root.style.setProperty("filter", "grayscale(100%");
+}
