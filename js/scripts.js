471ddde5cf2cd5f7f7696521190e8ec72ed2be68
santio89 <santiolais1989@gmail.com>
2021-12-18 17:56:33 -0300

    feat-color theme picker

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,7 @@ let footer = document.getElementById("footer");
 let footer__hr = document.getElementById("footer__hr")
 let particles = document.getElementById("particles-js")
 let root = document.documentElement;
+let vid = document.getElementById("landingVid");
 
 colorA.onclick = function(){
   colorC.classList.remove("color--active");
@@ -67,9 +68,9 @@ colorA.onclick = function(){
   footer.style.setProperty("background", "linear-gradient(45deg, #2b0053, #0fff87)");
   footer.style.setProperty("background-size", "500%");
   footer__hr.style.setProperty("background", "linear-gradient(270deg, #2b0053, #0fff87)");
-  particles.style.setProperty("background", "linear-gradient(135deg, rgba(43, 0, 83, 0.8), rgba(15, 255, 135, 0.8))");
+  particles.style.setProperty("background", "linear-gradient(135deg, rgba(43, 0, 83, .8), rgba(15, 255, 135, .8))");
   particles.style.setProperty("background-size", "500%");
-  root.style.setProperty("filter", "none");
+ /*  root.style.setProperty("filter", "none"); */
 }
 
 colorB.onclick = function(){
@@ -84,9 +85,9 @@ colorB.onclick = function(){
     footer.style.setProperty("background", "linear-gradient(45deg, #160800, #00b8f0)");
     footer.style.setProperty("background-size", "500%");
     footer__hr.style.setProperty("background", "linear-gradient(270deg, #160800, #00b8f0)");
-    particles.style.setProperty("background", "linear-gradient(135deg, rgba(22, 8, 0, 0.8), rgba(0, 184, 240, 0.8))")
+    particles.style.setProperty("background", "linear-gradient(135deg, rgba(22, 8, 0, .8), rgba(0, 184, 240, .8))")
     particles.style.setProperty("background-size", "500%");
-    root.style.setProperty("filter", "none");
+    /* root.style.setProperty("filter", "none"); */
 }
 
 colorC.onclick = function(){
@@ -97,5 +98,14 @@ colorC.onclick = function(){
   root.style.setProperty("--color-segundo","rgb(255, 255, 255)");
   root.style.setProperty("--color-primero-rgb","0, 0, 0");
   root.style.setProperty("--color-segundo-rgb","255, 255, 255");
-  root.style.setProperty("filter", "grayscale(100%");
+
+  footer.style.setProperty("background", "linear-gradient(45deg, rgb(0, 0, 0), rgb(255, 255, 255)");
+  footer.style.setProperty("background-size", "500%");
+  footer__hr.style.setProperty("background", "linear-gradient(270deg, rgb(0, 0, 0),rgb(255, 255, 255)");
+  particles.style.setProperty("background", "linear-gradient(135deg, rgba(0, 0, 0, .8), rgba(255, 255, 255, .8))")
+  particles.style.setProperty("background-size", "500%");
+  landingVid.style.setProperty("filter", "grayscale(100%)");
+  
+/*   footer.style.setProperty("filter", "grayscale(100%)");
+  particles.style.setProperty("filter", "grayscale(100%)"); */
 }
