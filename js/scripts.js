8fa7ec9592bac51775a7aadb21b973f1c5406c8b
santio89 <santiolais1989@gmail.com>
2021-12-25 19:18:20 -0300

    small fix

--- js/scripts.js
+++ js/scripts.js
@@ -180,20 +180,13 @@ colorC.onkeydown = function(e){
 
 /* FIN COLOR PICKER */
 
+/* hide nav */
+/* let header = document.getElementById("header");
 
-/* HIDE NAV BORDER ON TOP */
-
-$(function () {
-  var $win = $(window);
-
-  $win.scroll(function () {
-      if ($win.scrollTop() == 0)
-          alert('Scrolled to Page Top');
-      else if ($win.height() + $win.scrollTop()
-                     == $(document).height()) {
-          alert('Scrolled to Page Bottom');
-      }
-  });
-});
-
-/*FIN HIDE NAV BORDER ON TOP */
+if (window.scrollY>0){
+  header.style.setProperty("box-shadow", "0 .1rem 0rem 0 var(--color-primero)")
+}
+else{
+  header.style.setProperty("box-shadow", "none")
+} */
+/* fin hide nav */
