846896e711ad9e12a1e826a56700bcb26a4706f5
santio89 <santiolais1989@gmail.com>
2022-01-03 22:23:37 -0300

    style-removed particles js/added mouse trail

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,228 @@
+/* inicio mouse trail */
+const LINE_DURATION = 1;
+const LINE_WIDTH_START = 10;
+
+$(document).ready(function() {
+  enableDrawingCanvas();
+  resizeCanvas(window.innerWidth, window.innerHeight);
+});
+
+//////////////////////////
+// Variable definitions //
+//////////////////////////
+var active = true;
+
+var canvas;
+var context;
+
+var newWidth = 1000;
+var newHeight = 800;
+
+var spread = 2;
+
+var lineColor = 'rgb(117, 255, 186)';
+var lineDuration = LINE_DURATION;
+var lineFadeLinger = 1;
+var lineWidthStart = LINE_WIDTH_START;
+var fadeDuration = 50;
+var drawEveryFrame = 1; // Only adds a Point after these many 'mousemove' events
+
+var clickCount = 0;
+var frame = 0;
+
+var flipNext = true;
+
+var points = new Array();
+
+///////////////////////
+// Program functions //
+///////////////////////
+
+// Find canvas reference & enable listeners
+function enableDrawingCanvas() {
+  if (canvas === undefined) {
+    canvas = document.getElementById('myCanvas');
+    context = canvas.getContext('2d');
+    enableListeners();
+    init();
+  }
+}
+
+// Initialize animation start
+function init() {
+  draw();
+}
+
+// Draw current state
+function draw() {
+  if (active) {
+    animatePoints();
+    window.requestAnimFrame(draw);
+  }
+}
+
+// Update mouse positions
+function animatePoints() {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  var duration = lineDuration * 1000 / 60;
+  var point, lastPoint;
+
+  for (var i = 0; i < points.length; i++) {
+    point = points[i];
+
+    if (points[i - 1] !== undefined) {
+      lastPoint = points[i - 1];
+    } else {
+      lastPoint = points[i];
+    }
+
+    point.lifetime += 1;
+
+    if (point.lifetime > duration) {
+      points.splice(i, 1);
+      continue;
+    }
+
+    // Begin drawing stuff!
+    var inc = (point.lifetime / duration); // 0 to 1 over lineDuration
+    var dec = 1 - inc;
+
+    var spreadRate;
+    if (spread === 1) {
+      spreadRate = lineWidthStart / (point.lifetime * 2);
+    } // Lerp Decrease
+    if (spread === 2) {
+      spreadRate = lineWidthStart * (1 - inc);
+    } // Linear Decrease
+
+    var fadeRate = dec;
+
+    context.strokeStyle = lineColor;
+    context.lineJoin = "round";
+    context.lineWidth = spreadRate;
+
+    var distance = Point.distance(lastPoint, point);
+    var midpoint = Point.midPoint(lastPoint, point);
+    var angle = Point.angle(lastPoint, point);
+
+
+    context.beginPath();
+
+
+    context.moveTo(lastPoint.x, lastPoint.y);
+    context.lineTo(point.x, point.y);
+  
+      context.stroke();
+      context.closePath();
+
+  }
+
+  //if (points.length > 0) { console.log(spreadRate + "|" + points.length + " points alive."); }
+}
+
+function addPoint(x, y) {
+  flipNext = !flipNext;
+  var point = new Point(x, y, 0, flipNext);
+  points.push(point);
+}
+
+//////////////////////////////
+// Less Important functions //
+//////////////////////////////
+
+// RequestAnimFrame definition
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+// Update canvas dimensions based on input
+function resizeCanvas(w, h) {
+  if (context !== undefined) {
+    context.canvas.width = w;
+    context.canvas.height = h;
+
+    newWidth = w;
+    newHeight = h;
+  }
+}
+
+// Listeners for mouse and touch events
+function enableListeners() {
+
+  //********* Mouse Listeners *********//
+  $('#myCanvas').on('mousemove', function(e) {
+    if (frame === drawEveryFrame) {
+      addPoint(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      frame = 0;
+    }
+    frame++;
+  });
+
+  $('#myCanvas').on('mouseover', function(e) {});
+  $('#myCanvas').on('mouseleave', function(e) {});
+
+  //********* Touch Listeners *********//
+  $('#myCanvas').on('touchstart', function(e) {
+    var touch = e.touches[0];
+  });
+  $('#myCanvas').on('touchmove', function(e) {
+    var touch = e.touches[0];
+  });
+  $('#myCanvas').on('touchend', function(e) {});
+}
+
+
+// POINT CLASS
+// Cartersian location of where mouse location
+// was previously at. 
+// Used to draw arcs between Points.
+var Point = class Point {
+
+  // Define class constructor
+  constructor(x, y, lifetime, flip) {
+    this.x = x;
+    this.y = y;
+    this.lifetime = lifetime;
+    this.flip = flip;
+  }
+
+  // Get the distance between a & b
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  // Get the mid point between a & b
+  static midPoint(a, b) {
+    const mx = a.x + (b.x - a.x) * 0.5;
+    const my = a.y + (b.y - a.y) * 0.5;
+
+    return new Point(mx, my);
+  }
+
+  // Get the angle between a & b
+  static angle(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.atan2(dy, dx);
+  }
+
+  // Simple getter for printing
+  get pos() {
+    return this.x + "," + this.y;
+  }
+}
+
+/* fin mouse trail */
+
+
 /* inicio loader */
 let root = document.documentElement;
 let loader = document.getElementById("loaderWrapper");
@@ -75,6 +300,7 @@ colorA.onclick = function(){
   /* root.style.setProperty("--background","url('/media/backgroundA.svg')"); */
   /* particles.style.setProperty("background", "linear-gradient(135deg, rgba(43, 0, 83, .8), rgba(15, 255, 135, .8))");
   particles.style.setProperty("background-size", "400%"); */
+  lineColor = 'rgb(117, 255, 186)';
 }
 
 colorB.onclick = function(){
@@ -94,6 +320,7 @@ colorB.onclick = function(){
     /* root.style.setProperty("--background","url('/media/backgroundB.svg')"); */
     /* particles.style.setProperty("background", "linear-gradient(135deg, rgba(22, 8, 0, .8), rgba(0, 184, 240, .8))")
     particles.style.setProperty("background-size", "400%"); */
+    lineColor = 'rgb(87, 216, 255)';
 }
 
 colorC.onclick = function(){
@@ -113,6 +340,7 @@ colorC.onclick = function(){
   /* root.style.setProperty("--background","url('/media/backgroundC.svg')"); */
   /* particles.style.setProperty("background", "linear-gradient(135deg, rgba(0, 0, 0, .8), rgba(255, 255, 255, .8))")
   particles.style.setProperty("background-size", "400%"); */
+    lineColor = 'rgb(200, 200, 200)';
 }
 
 /* FIN COLOR PICKER */
@@ -129,3 +357,6 @@ else{
   header.style.setProperty("box-shadow", "none")
 } */
 /* fin hide nav */
+
+
+
