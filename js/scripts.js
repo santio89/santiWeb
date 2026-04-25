98428f2eafda866a5e49a088652b57d822305d50
santio89 <santiolais1989@gmail.com>
2026-04-24 22:37:06 -0300

    small fix

--- js/scripts.js
+++ js/scripts.js
@@ -1199,6 +1199,9 @@ function buildDoomFrameDoc(bundleUrl, theme) {
       margin: 0; padding: 0; height: 100%; width: 100%;
       overflow: hidden;
       font-family: ui-monospace, monospace;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-rendering: optimizeLegibility;
     }
     #dos { position: absolute; inset: 0; width: 100%; height: 100%; }
   </style>
